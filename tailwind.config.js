c1276cccc3eb5a9f78c1f8a3d98a7f3509c8815d
Ivan Chaplinskyi <chaplinskyi.ivan@gmail.com>
2024-06-10 15:12:16 +0300

    tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+      },
+      colors: {
+        black: "#0A0A0A",
+        neutralBlack: "#222222",
+        white: "#FFFFFF",
+        primary: "#D3E97A",
+        text: "#C7C7C7",
+      },
+      fontSize: {
+        base: '16px'
+      }
+    },
   },
   plugins: [],
-}
-
+};
